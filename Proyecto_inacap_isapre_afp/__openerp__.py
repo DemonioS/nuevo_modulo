e7ae5ca9ec5568deba197243a01b9dbc722d6921
DemonioS <alejandrowenzel@gmail.com>
2015-12-12 16:06:22 -0300

    modulos 3.0

---
+++ Proyecto_inacap_isapre_afp/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+ 
+{
+    'name' : 'ISAPRES y AFP de Chile',
+    'version' : '1.0',
+    'author' : 'Alejandro Wenzel',
+    'summary' : 'proyecto titulo',
+    'description' : 'lista de isapres para el sistema de recurso humanos de la empresa',
+    'depends' : [
+            'base',
+            ],
+    'data' : [              
+            'isapreAfp_view.xml', 
+            'isapreAfp_data.xml', 
+                ],
+    'installable' : True,
+    'aplication' : True,    
+}
---
+++ proyecto_inacap_indicadores/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+ 
+{
+    'name' : 'remuneracion de chile',
+    'version' : '1.0',
+    'author' : 'Alejandro Wenzel',
+    'summary' : 'proyecto titulo',
+    'aplication' : True,    
+    'description' : 'indicadores chilenos y remuneraciones ',
+    'depends' : [
+            'base',
+            ],
+    'data' : [              
+            'indicadores1_view.xml', 
+            'indicadores2_view.xml', 
+            'indicadores3_view.xml', 
+            'remuneraciones_view.xml', 
+              ],
+    'installable' : True,
+    'aplication' : True,    
+}
